ee8913c06a9374164b959995d9f7367bbf021c06
DmitriCastillo <160551322+DmitriCastillo@users.noreply.github.com>
2024-06-03 09:36:53 -0600

    Update File_Proyecto_3.py

--- File_Proyecto_3.py
+++ File_Proyecto_3.py
@@ -67,6 +67,8 @@ plt.title('Segundo cambio')
 
 # Realizar la prueba t de una muestra
 
+#   Prueba de Hipotesis H_0: \mu = 70
+
 print('prueba t','\n')
 #----------------------------------------------------------------------------
 print('prueba t de los datos iniciales')
@@ -90,7 +92,7 @@ print("")
 
 #----------------------------------------------------------------------------
 
-
+#   Prueba de Hipotesis H_0: \mu = 75
 print('prueba t para un 75%','\n')
 #----------------------------------------------------------------------------
 print('prueba t de los datos iniciales')
@@ -113,7 +115,9 @@ print(f'Valor p: {p_value_2}')
 
 #----------------------------------------------------------------------------
 print("")
-# Ajustar los datos a una normal, estimando la desviacion estandar
+
+
+# Ajustar los datos a una normal, estimando la desviacion estandar MLE (metodo empirico)
 
 print("Estimacion de la desviacion estandar",'\n')
 #----------------------------------------------------------------------------
@@ -121,16 +125,40 @@ print('Rendimiento de la maquina inicial')
 media_i = np.mean(datos_i)
 scale_inicial = stats.norm.fit(datos_i,floc=media_i)
 print(f'Estimacion de la desviacion estandar: {scale_inicial[1]}','\n')
+varianza_i = (scale_inicial[1])**2
+print(f'Varianza: {varianza_i}','\n')
 #----------------------------------------------------------------------------
 print('Rendimiento de la maquina en el primer cambio')
 media_1 = np.mean(datos_1)
 scale_1 = stats.norm.fit(datos_1,floc=media_1)
 print(f'Estimacion de la desviacion estandar: {scale_1[1]}','\n')
+varianza_1 = (scale_1[1])**2
+print(f'Varianza: {varianza_1}','\n')
 #----------------------------------------------------------------------------
 print('Rendimiento de la maquina en el segundo cambio cambio')
 media_2 = np.mean(datos_2)
 scale_2 = stats.norm.fit(datos_2,floc=media_2)
 print(f'Estimacion de la desviacion estandar: {scale_2[1]}','\n')
+varianza_2 = (scale_2[1])**2
+print(f'Varianza: {varianza_2}','\n')
 #----------------------------------------------------------------------------
+
+#   Calculo de la desivicion estandar metodo analitico 
+
+varianza_analitica_i = np.sum((datos_i - media_i) ** 2) / (len(datos_i) - 1)
+varianza_analitica_1 = np.sum((datos_1 - media_1) ** 2) / (len(datos_1) - 1)
+varianza_analitica_2 = np.sum((datos_2 - media_2) ** 2) / (len(datos_2) - 1)
+
+print('Varianza por el metodo analitico')
+print(f'Configuracion inicial: {varianza_analitica_i}\nPrimer cambio: {varianza_analitica_1}\nSegundo cambio: {varianza_analitica_2}\n')
+
+#   Calculo del porcentaje de error
+
+porcentaje_error_i= (np.abs(varianza_analitica_i - varianza_i)/varianza_analitica_i )*100
+porcentaje_error_1= (np.abs(varianza_analitica_1 - varianza_1)/varianza_analitica_1 )*100
+porcentaje_error_2= (np.abs(varianza_analitica_2 - varianza_2)/varianza_analitica_2 )*100
+
+print('Porcentaje de error de la varianza')
+print(f'Configuracion inicial: {porcentaje_error_i}%\nPrimer cambio: {porcentaje_error_1}%\nSegundo cambio: {porcentaje_error_2}%')
 #mostrar los graficos
 plt.show()
